0ec53cd491cc0a8408e502d862b980144153d0c3
mohmamadorabi <52682706+mohmamadorabi@users.noreply.github.com>
2025-01-24 00:02:53 +0100

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION['admin_id'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
+    <title>Admin Dashboard mohmed</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
